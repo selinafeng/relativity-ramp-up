4842a658af6fe1105c8072c7882dd51a442e9425
Isabel Li <ilyues@berkeley.edu>
2021-02-13 11:39:13 +1300

    multiple testsgit statusgit status

--- server/app.test.ts
+++ server/app.test.ts
@@ -21,6 +21,22 @@ jest.mock("@influxdata/influxdb-client", () => {
             ) {
               return MOCK_SPEED;
             }
+            if (
+              query ==
+              'from(bucket:"' +
+                bucket +
+                '") |> range(start: 0)  |> filter(fn: (r) => r._measurement == "altitude") '
+            ) {
+              return MOCK_ALT;
+            }
+            if (
+              query ==
+              'from(bucket:"' +
+                bucket +
+                '") |> range(start: 0)  |> filter(fn: (r) => r._measurement == "speed") |> difference(nonNegative: false, keepFirst: true)'
+            ) {
+              return MOCK_ACC;
+            }
           },
         };
       }
@@ -43,6 +59,36 @@ const MOCK_SPEED = [
   },
 ];
 
+const MOCK_ALT = [
+  {
+    result: "_result",
+    table: 0,
+    _start: "1970-01-01T00:00:00Z",
+    _stop: "2021-02-12T22:00:49.737656103Z",
+    _time: "2021-02-05T21:14:21.0460741Z",
+    _value: 0,
+    _field: "value",
+    _measurement: "altitude",
+    flight: "1",
+    host: "host1",
+  },
+];
+
+const MOCK_ACC = [
+  {
+    result: "_result",
+    table: 0,
+    _start: "1970-01-01T00:00:00Z",
+    _stop: "2021-02-12T22:02:24.838859679Z",
+    _time: "2021-02-05T21:14:21.0461067Z",
+    _value: null,
+    _field: "value",
+    _measurement: "speed",
+    flight: "1",
+    host: "host1",
+  },
+];
+
 describe("API functionality", () => {
   test("GET /speed correctly fetches speed", async (done) => {
     await request(app)
@@ -50,6 +96,26 @@ describe("API functionality", () => {
       .expect(200)
       .expect("Content-Type", /json/)
       .expect(MOCK_SPEED);
+
+    done();
+  });
+
+  test("GET /acceleration correctly fetches acceleration", async (done) => {
+    await request(app)
+      .get("/acceleration")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .expect(MOCK_ACC);
+
+    done();
+  });
+
+  test("GET /altitude correctly fetches altitude", async (done) => {
+    await request(app)
+      .get("/altitude")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .expect(MOCK_ALT);
 
     done();
   });
