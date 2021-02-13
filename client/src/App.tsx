ec60078ae2e569cef7853fb7b1126197ecd4f92c
Ranon Larpcharern <ranonl@berkeley.edu>
2021-02-13 13:58:55 +0700

    request setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,77 @@
-import Dashboard from './components/Dashboard'
+import React from "react";
+import { TimeSeries } from "pondjs";
+import {
+  Charts,
+  ChartContainer,
+  ChartRow,
+  YAxis,
+  LineChart
+} from "react-timeseries-charts";
 
 function App() {
+
+  const [data, setData] = React.useState(null);
+  const [loaded, setLoaded] = React.useState(false);
+
+  // Format of data from endpoint 
+  interface Data {
+    _result: string
+    table: number
+    start: string
+    stop: string
+    value: number,
+    field: string
+    measurement: string
+    flight: string
+    host: string
+  }
+
+  interface HttpResponse<T> extends Response {
+    parsedBody?: T;
+  }
+
+  // Retrieves data; parses body 
+  async function http<T>(
+    request: RequestInfo
+  ): Promise<HttpResponse<T>> {
+    const response: HttpResponse<T> = await fetch(request);
+    try {
+      response.parsedBody = await response.json();
+    } catch (ex) { }
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response;
+  }
+
+  // Consumer 
+  async function getData() {
+    let response: HttpResponse<Data[]>;
+    try {
+      response = await http<Data[]>("http://localhost:3000/acceleration")
+      console.log("response: ", response);
+    } catch (response) {
+      console.log("Error: ", response);
+    }
+  }
+
+  function rawToFormattedData(res: Response) {
+    // setData(getData()); 
+  }
+
+  function helper() {
+    if (loaded) {
+
+    }
+  }
+
+  getData();
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <div>{getData()}</div>
+    // getData()
   )
+
 }
 
-export default App;
+export default App; 
--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,7 +1,43 @@
+import {
+    Charts,
+    ChartContainer,
+    ChartRow,
+    YAxis,
+    LineChart
+} from "react-timeseries-charts";
+
 function Dashboard() {
+
+    // Format of data from endpoint 
+    interface data {
+        _result: string
+        table: number
+        start: string
+        stop: string
+        value: number,
+        field: string
+        measurement: string
+        flight: string
+        host: string
+    }
+
+    async function getData() {
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/todos"
+        );
+        const body = await response.json();
+        return body;
+    }
+
+    function rawToFormattedData(res: Response) {
+
+    }
+
     return (
-        <div>Your visualizations go here</div>
+        // <div>Your visualizations go here</div>
+        getData()
     )
+
 }
 
 export default Dashboard;
