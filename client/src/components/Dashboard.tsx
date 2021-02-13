aae46e5cb71848af77d04bb5459b96742751665d
Eric Hao <ehao@berkeley.edu>
2021-02-13 15:36:33 -0800

    progress

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -9,10 +9,12 @@ import {
 import { Component } from "react";
 import { ReactComponent } from "*.svg";
 import { Http2ServerRequest } from "http2";
+import React from "react";
+import { TimeSeries, Time, TimeRange } from "pondjs";
 
 export default class DashBoard extends Component<
   {},
-  { result: dataRocket | null }
+  { result: TimeSeries<Time> | null }
 > {
   constructor(props) {
     super(props);
@@ -24,44 +26,105 @@ export default class DashBoard extends Component<
   //run once when the component is first rendered
   async componentDidMount() {
     console.log("ack");
-    const data = await this.http("http://localhost:8000/engines");
+    var altData = await this.http("http://localhost:8000/altitude");
 
     // const response = await dataChangeType(
     //   fetch("http://localhost:8000/engines")
     // );
 
-    console.log(data);
+    var series: TimeSeries<Time> = this.formatData(altData, "altitude");
+    console.log(series);
 
-    this.setState({ result: data });
+    this.setState({ result: altData });
   }
   //put graphs here
   render() {
-    return <div>Hello</div>;
+    // return <div>Hello</div>;
+
+    //set up the timeRange
+    var beginTime: Time = new Time("2021-02-05T21:13:29.690Z");
+    var endTime: Time = new Time("2021-02-05T21:18:29.690");
+    var range: TimeRange = new TimeRange(beginTime, endTime);
+
+    if (this.state.result == null) {
+      return <div> Loading!</div>;
+    } else {
+      return (
+        // takes a TimeRange object, need to figure out how to find it
+        // t TimeRange = new TimeRanges
+
+        <ChartContainer timeRange={range} width={800}>
+          <ChartRow height="200">
+            <YAxis id="axis1" label="AUD" min={0.5} max={1.5} width="60" />
+            <Charts>
+              <LineChart
+                axis="axis1"
+                series={this.state.result}
+                column={["aud"]}
+              />
+              <LineChart
+                axis="axis2"
+                series={this.state.result}
+                column={["euro"]}
+              />
+            </Charts>
+            <YAxis id="axis2" label="Euro" min={0.5} max={2.0} width="80" />
+          </ChartRow>
+        </ChartContainer>
+      );
+    }
   }
 
-  //
+  //function that helps with the request
   async http(request: RequestInfo): Promise<any> {
     const response = await fetch(request);
     const body = await response.json();
     return body;
   }
+
+  formatData(data: any, nameP: string) {
+    // console.log("in format data:");
+    // console.log(data);
+    // console.log(data[0]["_start"]);
+    // data = data.map((point) => [point["_start"], point["_value"]]);
+    // console.log()
+    // const series: TimeSeries<Time> = new TimeSeries({
+    //   name: nameP,
+    //   columns: ["time", "value"],
+    //   points: data,
+    // });
+
+    const series = new TimeSeries({
+      name: "traffic",
+      columns: ["time", "in", "out"],
+      points: [
+        [1400425947000, 52, 12],
+        [1400425948000, 18, 42],
+        [1400425949000, 26, 81],
+        [1400425950000, 93, 11],
+      ],
+    });
+    console.log(series);
+
+    return series;
+  }
 }
 
-function dataChangeType(res: Promise<Response>) {
-  console.log(res);
-  var x: dataRocket = {
-    result: "dummy r",
-    table: 1,
-    _start: "dummy r",
-    _stop: "dummy r",
-    _value: 1,
-    _measurement: "dummy r",
-    flight: "dummy r",
-    host: "dummy r",
-  };
+// function dataChangeType(res: Promise<Response>) {
+//   console.log(res);
+//   var x: dataRocket = {
+//     result: "dummy r",
+//     table: 1,
+//     _start: "dummy r",
+//     _stop: "dummy r",
+//     _value: 1,
+//     _measurement: "dummy r",
+//     flight: "dummy r",
+//     host: "dummy r",
+//   };
 
-  return x;
-}
+//   return x;
+// }
 
 // async function http<T>(request: RequestInfo): Promise<T> {
 //   const response = await fetch(request);
@@ -69,16 +132,16 @@ function dataChangeType(res: Promise<Response>) {
 //   return body;
 // }
 
-interface dataRocket {
-  result: string;
-  table: number;
-  _start: string;
-  _stop: string;
-  _value: number;
-  _measurement: string;
-  flight: string;
-  host: string;
-}
+// interface dataRocket {
+//   result: string;
+//   table: number;
+//   _start: string;
+//   _stop: string;
+//   _value: number;
+//   _measurement: string;
+//   flight: string;
+//   host: string;
+// }
 
 // async function http<dataRocket>(request: RequestInfo): Promise<any> {
 //   const response = await fetch(request);
@@ -121,16 +184,3 @@ interface dataRocket {
 //     console.log("rip");
 //   }
 // }
-
-//   return (
-//     <ChartContainer timeRange={s1.timerange()} width={800}>
-//       <ChartRow height="200">
-//         <YAxis id="axis1" label="AUD" min={0.5} max={1.5} width="60" />
-//         <Charts>
-//           <LineChart axis="axis1" series={series1} column={["aud"]} />
-//           <LineChart axis="axis2" series={series2} column={["euro"]} />
-//         </Charts>
-//         <YAxis id="axis2" label="Euro" min={0.5} max={2.0} width="80" />
-//       </ChartRow>
-//     </ChartContainer>
-//   );
