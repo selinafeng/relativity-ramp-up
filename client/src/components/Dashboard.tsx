14ffc32237498ac0a0ef794cf3e0e581094b151a
Ranon Larpcharern <ranonl@berkeley.edu>
2021-02-14 15:00:23 +0700

    for selina! :)

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,17 +1,31 @@
 import React from "react";
-import { TimeSeries } from "pondjs";
+import { TimeSeries, timeEvent, time } from "pondjs";
+import * as Immutable from 'immutable';
 import {
     Charts,
+    BarChart,
     ChartContainer,
     ChartRow,
+    Resizable,
+    Styler,
     YAxis,
     LineChart
 } from "react-timeseries-charts";
+import { TimeEventObject } from "pondjs/lib/event";
 
 function Dashboard() {
 
     const [data, setData] = React.useState(null);
-    const [loaded, setLoaded] = React.useState(false);
+    const tempSeries = new TimeSeries({
+        name: "temp",
+        columns: ["time", "value"],
+        points: [
+            ["1h-412568", 0.01],
+            ["1h-412569", 0.13],
+        ],
+    });
+
+    const [altitudeData, setAltitudeData] = React.useState(tempSeries);
 
     // Format of data from endpoint 
     interface Data {
@@ -51,23 +65,67 @@ function Dashboard() {
         let response: HttpResponse<Data[]>;
         try {
             response = await http<Data[]>(url);
-            console.log("response", response);
-            return data;
+            if (response.parsedBody != undefined) {
+                const body: Data[] = response.parsedBody;
+                console.log(body);
+                const data = timeSeriesDataFormat(body, "hi");
+                return data;
+            }
         } catch (response) {
             console.log("Error: ", response);
         }
     }
 
+    // Formats the json data into a format readible by TimeSeries 
+    // function timeSeriesDataFormat(jsonData: Data[], varName: string) {
+    //     let pointsRaw: any[][];
+    //     pointsRaw = [];
+    //     for (let i = 0; i < jsonData.length; i++) {
+    //         const date = new Date(jsonData[i]["_time"]);
+    //         // const e = new Event(date.getTime(), jsonData[i]["_value"]);
+    //         pointsRaw.push([date.getTime(), jsonData[i]["_value"]]);
+    //     }
+    //     const series = new TimeSeries({
+    //         name: varName,
+    //         columns: ["time", "value"],
+    //         points: pointsRaw
+    //     });
+    //     setAltitudeData(series);
+    //     console.log(series.toString());
+    // }
+
+    // Formats the json data into a format readible by TimeSeries 
+    function timeSeriesDataFormat(jsonData: Data[], varName: string) {
+        let pointsRaw: any[];
+        pointsRaw = [];
+        for (let i = 0; i < jsonData.length; i++) {
+            const date = new Date(jsonData[i]["_time"]);
+            // const e = new Event(date.getTime(), jsonData[i]["_value"]);
+            // pointsRaw.push([date.getTime(), jsonData[i]["_value"]]);
+            pointsRaw.push(timeEvent(time(date), Immutable.Map({ value: 14 })));
+        }
+        const series = new TimeSeries({
+            name: varName,
+            columns: ["time", "value"],
+            points: pointsRaw
+        });
+        setAltitudeData(series);
+        console.log(series.timerange().toString());
+    }
+
+    // Peforms all neecssary functions on all 3 endpoints 
+    // function timeSeriesFormatAll() {
+    //     setAltitudeData(timeSeriesDataFormat(____)); 
+    // }
+
+    function renderAllCharts() {
+
+    }
+
     getData("http://localhost:8000/altitude");
 
     return (
-        <div>
-            <h3>
-                Visualization
-      </h3>
-            {/* {getData()} */}
-        </div>
-        // getData()
+        <div>hi</div>
     )
 
 }
